33edd337d420da35cf2206abe707e47f890db04b
Bernardo Hernandez <Bhdzhdz@gmail.com>
2021-02-03 22:49:09 -0600

    Create M.cpp

---
+++ RPC/2021/01/M.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define all(a) a.begin(), a.end()
+
+typedef long long ull;
+typedef unsigned long long ll;
+typedef pair<ull, ull> pii;
+typedef vector<ull> vi;
+
+#define MAX 1000100
+#define MOD 2147483647
+
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
+}
+
+const ll mod = MOD; // change to something else
+struct Mod {
+	ll x;
+	Mod(ll xx) : x(xx) {}
+	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
+	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
+	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
+	Mod operator/(Mod b) { return *this * invert(b); }
+	Mod invert(Mod a) {
+		ll x, y, g = euclid(a.x, mod, x, y);
+    return Mod((x + mod) % mod);
+	}
+	Mod operator^(ll e) {
+		if (!e) return Mod(1);
+		Mod r = *this ^ (e / 2); r = r * r;
+		return e&1 ? *this * r : r;
+	}
+};
+
+vector<ull> a;
+vector<ull> b;
+ull n;
+
+void solve(){
+    
+    cin>>n;
+    n%=MOD;
+
+    Mod ans = (Mod(n)*Mod(n+1)*Mod(n + 2)) / Mod(6);
+
+    cout << ans.x << '\n';
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
