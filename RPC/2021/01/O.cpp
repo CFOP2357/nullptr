2cdde7ddb3385364a857984ea1fde03a685e9f59
Bernardo Hernandez <Bhdzhdz@gmail.com>
2021-02-03 22:40:07 -0600

    Create O.cpp

---
+++ RPC/2021/01/O.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <map>
+#include <set>
+
+#include <stack>
+#include <queue>
+
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ull,null_type,less<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+typedef tree<ull,null_type,less_equal<ull>,rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+#define findBO find_by_order
+#define findBK order_of_key
+*/
+
+#define all(a) a.begin(), a.end()
+
+typedef long long ull;
+typedef pair<ull, ull> pii;
+typedef vector<ull> vi;
+
+#define MAX 1000100
+#define MOD 1000000007
+
+vector<ull> a;
+vector<ull> b;
+ull n;
+
+set<ull> fibNumber;
+void solve(){
+    int a, b;
+    cin >> a >> b;
+
+    if(a - b >=  10){
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
